3d2d8b4183b6182af332a16d6cbb3501be8a3c08
sarthaksharma39 <busharma2003@gamil.com>
2023-07-18 23:35:49 +0530

    Second test

---
+++ demo2.cpp
@@ -0,0 +1,2 @@
+//This is to check if I write the code today and upload it tomorrow then what would happen 
+//Will it works or not?
---
+++ firstjava.java
@@ -0,0 +1,9 @@
+/**
+ * firstjava
+ */
+public class firstjava {
+
+    public static void main(String[] args) {
+        System.out.println("Thank you subrat");
+    }
+}
