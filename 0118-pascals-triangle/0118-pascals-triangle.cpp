a6c8537ca9cb6c6254c19e0cff33fad4238149d0
Sarthak sharma <107210105+sarthaksharma39@users.noreply.github.com>
2023-12-20 22:25:56 +0530

    Time: 4 ms (25.62%), Space: 7.3 MB (12.10%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int nCr(int n, int r) {
-    long long res = 1;
+    vector<int> generateRow(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
 
-    // calculating nCr:
-    for (int i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
     }
-    return (int)(res);
+    return ansRow;
 }
     vector<vector<int>> generate(int numRows) {
     vector<vector<int>> ans;
 
-    //Store the entire pascal's triangle:
+    //store the entire pascal's triangle:
     for (int row = 1; row <= numRows; row++) {
-        vector<int> tempLst; // temporary list
-        for (int col = 1; col <= row; col++) {
-            tempLst.push_back(nCr(row - 1, col - 1));
-        }
-        ans.push_back(tempLst);
+        ans.push_back(generateRow(row));
     }
     return ans;
-
     }
 };
